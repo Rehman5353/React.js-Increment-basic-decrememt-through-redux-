6f939f4bd3a79d9ae0527186aed039364986d464
Abdul Rehman <rehman.n@dplit.com>
2022-09-09 18:54:34 +0500

    changes

--- src/reducers/upDown.js
+++ src/reducers/upDown.js
@@ -4,6 +4,7 @@ const changeTheNumber = ( state  = 0 , action ) => {
     switch (action.type) {
         case "INCREMENT": return state + 1;
         case "DECREMENT": return state - 1;
+        
         default :  return state;
     }
 
